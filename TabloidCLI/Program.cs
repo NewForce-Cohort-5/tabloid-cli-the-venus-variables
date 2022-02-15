a3bc1d780c89731cae58b208d7b3e7df37298cb5
cresuta <60229891+cresuta@users.noreply.github.com>
2022-02-15 15:47:34 -0500

    bg color completed, will work on moving logic into separate method to call in Main()

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -31,6 +31,94 @@ _|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|
 ___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|__
             ");
 
+
+            // Get an array with the values of ConsoleColor enumeration members.
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            // Save the current background color.
+            ConsoleColor currentBackground = Console.BackgroundColor;
+            // Display each background color except the one that matches the current foreground color.
+            Console.WriteLine("Choose a color to set the background color. Press '0' to keep the color as is.");
+            // int choice;
+            for (var i = 0; i < colors.Length; i++)
+            {
+                if (colors[i] == currentBackground) continue;
+                Console.WriteLine($"{i}) {colors[i]}");
+
+            }
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "0":
+                    break;
+                case "1": 
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.Clear();
+                    break;
+                case "2":
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.Clear();
+                    break;
+                case "3":
+                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    Console.Clear();
+                    break;
+                case "4":
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Clear();
+                    break;
+                case "5":
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.Clear();
+                    break;
+                case "6":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "7":
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.Clear();
+                    break;
+                case "8":
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.Clear();
+                    break;
+                case "9":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Clear();
+                    break;
+                case "10":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Clear();
+                    break;
+                case "11":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.Clear();
+                    break;
+                case "12":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    break;
+                case "13":
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.Clear();
+                    break;
+                case "14":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "15":
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    break;
+            }
+
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
             while (ui != null)
