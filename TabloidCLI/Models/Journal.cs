3ccc7098722dce745261a433142925cf3c13d199
cresuta <60229891+cresuta@users.noreply.github.com>
2022-02-15 13:47:50 -0500

    journal changes in manager, detail manager, journal, and main menu

--- TabloidCLI/Models/Journal.cs
+++ TabloidCLI/Models/Journal.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TabloidCLI.Models
 {
-    internal class Journal
+    public class Journal
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        
     }
 }
--- TabloidCLI/UserInterfaceManagers/JournalDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/JournalDetailManager.cs
@@ -1,12 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    internal class JournalDetailManager
+    internal class JournalDetailManager : IUserInterfaceManager
     {
+        private IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private int _journalId;
+
+        public JournalDetailManager(IUserInterfaceManager parentUI, string connectionString, int journalId)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _journalId = journalId;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Journal journal = _journalRepository.Get(_journalId);
+            Console.WriteLine($"{journal.Title} Details");
+            Console.WriteLine(" 1) View");
+            Console.WriteLine(" 2) View Blog Posts");
+            Console.WriteLine(" 3) Add Tag");
+            Console.WriteLine(" 4) Remove Tag");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    View();
+                    return this;
+                case "2":
+                    ViewBlogPosts();
+                    return this;
+                case "3":
+                    AddTag();
+                    return this;
+                case "4":
+                    RemoveTag();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void View()
+        {
+            Author author = _authorRepository.Get(_authorId);
+            Console.WriteLine($"Name: {author.FullName}");
+            Console.WriteLine($"Bio: {author.Bio}");
+            Console.WriteLine("Tags:");
+            foreach (Tag tag in author.Tags)
+            {
+                Console.WriteLine(" " + tag);
+            }
+            Console.WriteLine();
+        }
+
+        private void ViewBlogPosts()
+        {
+            List<Post> posts = _postRepository.GetByAuthor(_authorId);
+            foreach (Post post in posts)
+            {
+                Console.WriteLine(post);
+            }
+            Console.WriteLine();
+        }
+
+        private void AddTag()
+        {
+            Author author = _authorRepository.Get(_authorId);
+
+            Console.WriteLine($"Which tag would you like to add to {author.FullName}?");
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                Tag tag = tags[choice - 1];
+                _authorRepository.InsertTag(author, tag);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection. Won't add any tags.");
+            }
+        }
+
+        private void RemoveTag()
+        {
+            Author author = _authorRepository.Get(_authorId);
+
+            Console.WriteLine($"Which tag would you like to remove from {author.FullName}?");
+            List<Tag> tags = author.Tags;
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                Tag tag = tags[choice - 1];
+                _authorRepository.DeleteTag(author.Id, tag.Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection. Won't remove any tags.");
+            }
+        }
     }
 }
